5037158d496e80af55985e6614bf247a7e0b0183
zhoucheng <zhoucheng133@outlook.com>
2025-08-16 19:48:24 +0800

    完成基本功能

---
+++ app.py
@@ -0,0 +1,64 @@
+import sqlite3
+from fastapi import FastAPI
+
+class Counter:
+    def __init__(self) -> None:
+        conn = sqlite3.connect('data.db')
+        c =conn.cursor()
+        c.execute('''
+        CREATE TABLE IF NOT EXISTS counter (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            count INTEGER NOT NULL
+        )
+        ''')
+        conn.commit()
+        c.execute('SELECT COUNT(*) FROM counter')
+        if c.fetchone()[0] == 0:
+            c.execute('INSERT INTO counter (count) VALUES (0)')
+            conn.commit()
+
+    def add(self):
+        conn = sqlite3.connect('data.db')
+        c =conn.cursor()
+        c.execute('''
+            UPDATE counter
+            SET count = count + 1
+            WHERE rowid = 1;
+        ''')
+        conn.commit()
+    
+    def get(self):
+        conn = sqlite3.connect('data.db')
+        c =conn.cursor()
+        c.execute('SELECT count FROM counter WHERE rowid = 1;')
+        row = c.fetchone()
+        if row:
+            return row[0]
+        return None
+    
+    def clear(self):
+        conn = sqlite3.connect('data.db')
+        c =conn.cursor()
+        c.execute('''
+            UPDATE counter
+            SET count = 0
+            WHERE rowid = 1;
+        ''')
+        conn.commit()
+
+app = FastAPI()
+counter=Counter()
+
+@app.post("/add")
+def add():
+    counter.add()
+    return {"ok": True}
+
+@app.get("/get")
+def get():
+    return counter.get()
+
+@app.post("/clear")
+def clear():
+    counter.clear()
+    return {"ok": True}
